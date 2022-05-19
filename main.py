3f381b14af5499f0b18d01c48e3db49416192bc2
sara <sara.cuy.ramirez@gmail.com>
2022-05-18 22:40:37 -0500

    initialcommit

--- main.py
+++ main.py
@@ -19,25 +19,25 @@ async def up():
     
     GPIO.output(salida1,1)
     GPIO.output(salida2,0)
-    return {"salida1: " + GPIO.input(salida1)+ "salida2: " + GPIO.input(salida2)}
+    return {"salida1: " + str(GPIO.input(salida1))+ "salida2: " + str(GPIO.input(salida2))}
 
 @app.get("/down")
 async def down():
     GPIO.output(salida1,0)
     GPIO.output(salida2,1)
-    return {"salida1: " + GPIO.input(salida1)+ "salida2: " + GPIO.input(salida2)}
+    return {"salida1: " + str(GPIO.input(salida1))+ "salida2: " + str(GPIO.input(salida2))}
 
 @app.get("/stop")
 async def stop():
     GPIO.output(salida1,1)
     GPIO.output(salida2,1)
-    return {"salida1: " + GPIO.input(salida1)+ "salida2: " + GPIO.input(salida2)}
+    return {"salida1: " + str(GPIO.input(salida1))+ "salida2: " + str(GPIO.input(salida2))}
 
 
 @app.get("/auto")
 async def auto():
     GPIO.output(salida1,0)
     GPIO.output(salida2,0)
-    return {"salida1: " + GPIO.input(salida1)+ "salida2: " + GPIO.input(salida2)}
+    return {"salida1: " + str(GPIO.input(salida1))+ "salida2: " + str(GPIO.input(salida2))}
 
 
